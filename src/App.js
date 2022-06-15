3f704409b129bd9ef7f52c82e9e7b5815b1fd6c7
dmknapp2385 <dmknapp2385@gmail.com>
2022-06-15 12:20:33 -0700

    updated icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Homepage from './Components/Homepage';
+import Nav from './Components/Nav';
+import Works from './Components/Works';
+import Contact from './Components/Contact';
+import {useSpring, config} from 'react-spring';
+
 
 function App() {
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: config.molasses,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Nav props={props}></Nav>
+      <Router>
+        <Routes>
+          <Route path="/"  element={<Homepage props={props}/>}>
+          </Route>
+          <Route path="/works"  element={<Works/>}>
+          </Route>
+          <Route path="/contact"  element={<Contact props={props}/>}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
